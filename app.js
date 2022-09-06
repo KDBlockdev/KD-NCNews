a10a049473dd6c8d5f748a269363fccd35b2d783
Kieron <kdblockdev@gmail.com>
2022-09-06 14:58:12 +0100

    adding error handling to app.js

--- app.js
+++ app.js
@@ -11,5 +11,27 @@ app.get("/api", getMessage);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/users", getUsers);
+
+//Error Handling Middleware
+// Standard Errors
+app.use((err, req, res, next) => {
+    if (err.status) {
+      res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+  });
+  //PSQL thrown errors - 400
+  app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+      res.status(400).send({ msg: 'Invalid input' });
+    } else next(err);
+  });
+  //Custom Errors - 500
+  app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ msg: 'Internal Server Error' });
+  });
 
 module.exports = app;
